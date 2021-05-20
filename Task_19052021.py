a20bc1a121b81c7a2460bcf7e16efd6af442dab6
Afshana123 <nKafuk2JTFW!ATJ>
2021-05-20 09:24:39 +0100

    Task File Updated

--- Task_19052021.py
+++ Task_19052021.py
@@ -1,13 +1,18 @@
+# Promts User to enter their name and welcomes user
 Name = str(input("Please enter your name: "))
 print("Hello " + Name)
 
+# Promts User to enter their age
 Age = int(input("Please enter your age: "))
 
+# Promts user to enter their month of birth
 Month_of_birth = int(input("Please enter the month you were born in numerical format e.g '1': "))
 
+# Calculatiom forth their date of birth based on the month they were born
 Birthday_passed_this_year = 2021 - Age
 Birthday_not_passed_this_year = 2021 - Age + 1
 
+# An if, else statement which allows the correct calculation to be made based on their month of birth
 if (Month_of_birth <= 6):
     print("OMG " + Name + ", " + "you are " + str(Age) + " years old " + "so you were born in " + str(Birthday_passed_this_year))
 else:
